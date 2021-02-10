7f7ef295c46b20661c3278259460b1bcaeb76ddb
Marlee-Labs <Marlee-Labs@web.de>
2021-02-10 08:41:45 +0100

    Added Clipboard

--- src/de/kreuzbe/movingMouse/io/Sender.java
+++ src/de/kreuzbe/movingMouse/io/Sender.java
@@ -17,22 +17,6 @@ public class Sender extends IoManager {
         server.setInputConsumer(this::processEvent);
     }
 
-    @Override
-    public void eventDispatched(AWTEvent event) {
-        if (event instanceof MouseEvent) {
-            MouseEvent me = (MouseEvent) event;
-            if (me.getXOnScreen() < 10 && hasFocus) {
-                hasFocus = false;
-                getFrame().setExtendedState(Frame.MAXIMIZED_BOTH);
-                getRobot().mouseMove((int) tk.getScreenSize().getWidth(), me.getYOnScreen());
-            } else if (me.getXOnScreen() > tk.getScreenSize().getWidth() - 10 && !hasFocus) {
-                hasFocus = true;
-                getFrame().setBounds(0, 0, 10, (int) tk.getScreenSize().getHeight());
-                getRobot().mouseMove(10, me.getYOnScreen());
-            }
-        }
-        super.eventDispatched(event);
-    }
 
     @Override
     public void send(Object o) {
