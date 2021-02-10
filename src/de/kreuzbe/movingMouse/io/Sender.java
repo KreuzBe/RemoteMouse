72d75af2ca9af9a116dbd8ee8c7404647a63aef8
Marlee-Labs <Marlee-Labs@web.de>
2021-02-10 07:44:01 +0100

    Added Clipboard

--- src/de/kreuzbe/movingMouse/io/Sender.java
+++ src/de/kreuzbe/movingMouse/io/Sender.java
@@ -2,26 +2,16 @@ package de.kreuzbe.movingMouse.io;
 
 import de.kreuzbe.movingMouse.net.Server;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.awt.event.AWTEventListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseWheelEvent;
 import java.io.IOException;
 
 public class Sender extends IoManager {
-
-    private Server server;
+    private final Server server;
 
     public Sender(Server server) {
-        super();
         this.server = server;
         server.setInputConsumer(this::processEvent);
     }
+
 
     @Override
     public void send(Object o) {
@@ -31,6 +21,4 @@ public class Sender extends IoManager {
             e.printStackTrace();
         }
     }
-
-
 }
